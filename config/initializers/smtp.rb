a090ce6d48acaf8a8d3fa3a5e6885450dee65cc9
enod <slava.markevich@gmail.com>
2012-11-11 22:24:34 +0300

    add confirmable module to devise. Configure smtp settings

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'example.com',
+  :user_name            => 'mailer@nicedrawing.mygbiz.com',
+  :password             => '1231231admin',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
