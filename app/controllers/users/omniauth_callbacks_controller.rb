87539dfce0e5525a067081191c2a793e55546007
enod home <slava.markevich@gmail.com>
2012-11-20 20:38:32 +0300

    skip verification of email if user sign up throw facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,9 @@
-class Users::OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    sign_in_and_redirect user, :event => :authentication #this will throw if @user is not activated
+    set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
   end
 
   def vkontakte
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,14 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = find_by_provider_and_uid auth.provider, auth.uid
     unless user
-      user = create(name:auth.extra.raw_info.name,
+      user = new(name:auth.extra.raw_info.name,
                            provider:auth.provider,
                            uid:auth.uid,
                            email:auth.info.email,
                            password:Devise.friendly_token[0,20]
                            )
+      user.skip_confirmation!
+      user.save!
     end
     user
   end
