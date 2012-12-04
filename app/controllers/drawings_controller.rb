1471419d44a11013520378d61c683c52edfa071a
Sergei Baranov <onemur@gmail.com>
2012-12-04 23:32:03 +0300

    Added drawing categories controller.

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,12 +1,19 @@
 class DrawingsController < ApplicationController
   before_filter :check_owner, only: [:edit]
+  
   def index
-    @user = User.find(params[:user_id])
-    @drawings = @user.drawings
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @drawings = @user.drawings
+    elsif params[:drawing_category_id]
+      @drawings = DrawingCategory.find(params[:drawing_category_id]).drawings
+    end
   end
+
   def new
     @drawing = Drawing.new
   end
+
   def create
     @drawing = Drawing.new.tap do |d|
        d.path = params[:drawing][:path]
@@ -19,13 +26,17 @@ class DrawingsController < ApplicationController
       render action: :new
     end
   end
+
   def edit
     @drawing = Drawing.find params[:id]
   end
+
   private
+
   def check_owner
     unless Drawing.find(params[:id]).user == current_user
       redirect_to :back
     end
   end
+
 end
