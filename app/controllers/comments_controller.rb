6873f6ee19c262dc62654fef661b59f1bf1ac3cd
enod <slava.markevich@gmail.com>
2012-12-08 18:38:48 +0300

    restore comments saving =)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
     @drawing = Drawing.find(params[:comment][:commentable_id])
     @comment = Comment.build_from(@drawing, current_user.id, params[:comment][:body])
 
+    @comment.save
     #TODO: никаких редиректо, это должно обрабатываться аяксом
     redirect_to drawing_path(params[:drawing_id])
   end
