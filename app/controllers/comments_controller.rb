fd1041dc3b17e47f209d128ff802b3986d4879a2
enod <slava.markevich@gmail.com>
2012-12-08 18:15:58 +0300

    add comment to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
     @drawing = Drawing.find(params[:comment][:commentable_id])
     @comment = Comment.build_from(@drawing, current_user.id, params[:comment][:body])
 
+    #что это за иф такой??? зачем он сдесь?
     if @comment.save
       redirect_to drawing_path(params[:drawing_id])
     else
