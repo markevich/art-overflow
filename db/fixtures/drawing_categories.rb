d1f7b88e6a3109b16f25b4911fb07dc798d98198
EnOD <slava.markevich@gmail.com>
2013-01-16 14:35:27 -0800

    add seeds for nested categories

--- db/fixtures/drawing_categories.rb
+++ db/fixtures/drawing_categories.rb
@@ -1,7 +1,10 @@
 DrawingCategory.seed(:id,
-  {:name => "Digital art"},
-  {:name => "Traditional art"},
-  {:name => "Anime & Manga"},
+  {id: 1, name: 'Digital art'},
+  {id: 2, name: 'Traditional art'},
+  {id: 3, name: 'Anime & Manga'},
+  {id: 6, name: 'Naruto', parent_id: 3},
+  {id: 4, name: 'Self drawing', parent_id: 1},
+  {id: 5, name: 'Animals', parent_id: 1},
 )
 #friendly_id
 DrawingCategory.find_each(&:save)
