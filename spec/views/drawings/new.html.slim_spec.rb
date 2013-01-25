8b6f402aec5769ebe8e1453c6138b322b8b8b38a
EnOD <slava.markevich@gmail.com>
2013-01-25 14:13:27 -0800

    fix new drawing spec

--- spec/views/drawings/new.html.slim_spec.rb
+++ spec/views/drawings/new.html.slim_spec.rb
@@ -10,7 +10,7 @@ describe 'drawings/new.html.slim' do
       create(:drawing_category)
     end
     it { page.should have_field 'Имя' }
-    it { page.should have_select('drawing_drawing_category', with_options: [DrawingCategory.first.name]) }
+    it { page.should have_select('drawing_drawing_category_id', with_options: [DrawingCategory.first.name]) }
     it { page.should have_field 'Тэги' }
     it { page.should have_button 'Сохранить' }
   end
