0c2b898ecca4d98dc1377616a93f9160cca7ad5a
EnOD <slava.markevich@gmail.com>
2013-01-26 14:27:27 -0800

    add stubbing of remote url in step definitions

--- features/step_definitions/entities_steps.rb
+++ features/step_definitions/entities_steps.rb
@@ -1,3 +1,7 @@
 Given /^drawing category "(.*?)"$/ do |name|
   create(:drawing_category, name: name)
 end
+
+Given /^picture "(.*?)" exist$/ do |path|
+  DrawingUploader.any_instance.stub(:download!)
+end
