c42c521455b72ac5f649dc2161cd99e5870b2ebe
enod <slava.markevich@gmail.com>
2013-02-10 00:00:50 +0300

    save meta information for new drawing

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,6 +1,7 @@
 class Drawing < ActiveRecord::Base
   validates :name, :drawing_category_id, presence: true, on: :update
   validates :path, presence: true, on: :create
+  before_create :save_meta_info
 
   acts_as_commentable
   acts_as_viewable
@@ -10,8 +11,25 @@ class Drawing < ActiveRecord::Base
   belongs_to :drawing_category
 
   mount_uploader :path, DrawingUploader
+  
+  serialize :meta_info
 
   def comments
   	Comment.find_comments_for_commentable(self.class.base_class.name, self.id)
+  end
+
+  private
+  def save_meta_info
+    self.meta = get_meta_from(path).merge({
+      thumb: get_meta_from(path.thumb)
+    })
+  end
+
+  def get_meta_from picture
+    picture = ::MiniMagick::Image::read(File.binread(picture.file.file))
+    {
+      width: picture[:width],
+      height: picture[:height] 
+    }
   end
 end
