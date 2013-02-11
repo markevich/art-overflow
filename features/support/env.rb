c44705aeb44080b44547f0e25e0464d0cc47abef
Sergei Baranov <onemur@gmail.com>
2013-02-11 21:39:54 +0300

    truncate database for cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,7 +20,7 @@ Spork.each_run do
   ActionController::Base.allow_rescue = false
   
   begin
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   rescue NameError
     raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
   end
