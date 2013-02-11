e636717c77a5fcde57ab682a73492b0cd60b56f2
Sergei Baranov <onemur@gmail.com>
2013-02-11 22:43:34 +0300

    add checking of authorization in follow and unfollow methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   layout 'user'
   before_filter :set_model
+  before_filter :check_if_signed_in, only: [:follow, :unfollow]
   def drawings
     @drawings = @user.drawings
     render layout: 'application'
@@ -11,14 +12,17 @@ class UsersController < ApplicationController
   end
 
   def follow
-    #TODO: redirect_to authorize_path unless current_user
+
   end
 
   def unfollow
-    #TODO: redirect_to authorize_path unless current_user
   end
 
   private
+  def check_if_signed_in
+    render(status: 401, text: 'not authorized') unless user_signed_in?
+  end
+
   def set_model
     @user = User.find(params[:id])
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,33 @@
 require "rspec"
 
 describe UsersController do
-  let(:user) { create(:user) }
-  let(:valid_params) { {id: user.id} }
-  before do
-
-  end
+  let(:another_user) { create(:user, :another) }
   describe 'GET show' do
+    let(:valid_params) { {id: another_user.id} }
     it 'show 200 if user exist' do
       get :show, valid_params
       response.should be_success
     end
     it 'show 404 if user not exist' do
       expect { get :show, {id: 'bad'} }.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
+
+  describe 'POST follow' do
+    let(:current_user) { create(:user) }
+    let(:valid_params) { {id: another_user.id} }
+    before do
+      UsersController.stub(:current_user).and_return current_user
+    end
+
+    it 'show 401 if user not authorized' do
+      UsersController.stub(:current_user).and_return nil
+      post :follow, valid_params
+      response.status.should == 401
+    end
+
+    it 'makes current_user to follow another user' do
+
     end
   end
 end
