5d3a905e10290ba8574c894bec72e9dcad770431
Sergei Baranov <onemur@gmail.com>
2013-02-11 20:27:13 +0300

    add "another_user" factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    name                  "Testy enod"
+    name                  "Testy user"
     email                 "example@example.com"
     password              "please"
     password_confirmation "please"
     confirmed_at          Time.now
+
+    factory :another_user do
+      id                    200
+      name                  "Another user"
+      email                 "another@example.com"
+    end
+
   end
 end
