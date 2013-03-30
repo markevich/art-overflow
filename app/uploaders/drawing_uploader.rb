9c9e706674ac70d969626987bb5f9901e7884e2e
markevich <slava.markevich@gmail.com>
2013-03-30 16:32:13 +0300

    fix thumb size

--- app/uploaders/drawing_uploader.rb
+++ app/uploaders/drawing_uploader.rb
@@ -37,7 +37,7 @@ class DrawingUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [300, 200]
+    process :resize_to_fit => [280, 280]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
