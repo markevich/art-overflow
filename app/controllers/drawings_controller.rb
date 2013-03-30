7a6a1d1edfa935a16b3766c8380371db898093aa
markevich <slava.markevich@gmail.com>
2013-03-30 16:05:58 +0300

    drawing workaround

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -2,4 +2,17 @@ class DrawingsController < ApplicationController
   def index
     @drawings = Drawing.all
   end
+
+  def new
+    @drawing = Drawing.new
+  end
+
+  def create
+    Drawing.create drawing_params
+  end
+
+  private
+  def drawing_params
+    params.require(:drawing).permit(:name, :path)
+  end
 end
--- db/migrate/20130330124215_create_table_drawings.rb
+++ db/migrate/20130330124215_create_table_drawings.rb
@@ -1,4 +1,8 @@
 class CreateTableDrawings < ActiveRecord::Migration
   def change
+    create_table :drawings do |t|
+      t.string :name
+      t.string :path
+    end
   end
 end
