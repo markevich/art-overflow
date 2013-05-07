16a59a16015a0e1ba41c13f0e15f8d6f9bb28d25
markevich <slava.markevich@gmail.com>
2013-05-07 15:31:46 +0300

    new chema

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20130330124215) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
