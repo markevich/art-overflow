6e54f308a7cd49936af99aed7619df026a0fc664
markevich <slava.markevich@gmail.com>
2013-05-08 20:39:34 +0300

    fix beta invite

--- app/workers/beta_invite.rb
+++ app/workers/beta_invite.rb
@@ -1,10 +1,10 @@
 class BetaInvite
   include Sidekiq::Worker
 
-  def perform(lucky_user_id, executor_id)
+  def perform(invite_id, executor_id)
     executor = User.find(executor_id)
-    user = User.find(lucky_user_id)
-    return unless User.where(email: user.email).empty?
-    user.invite!({email: user.email}, executor)
+    invite = Invite.find(invite_id)
+    return unless User.where(email: invite.email).empty?
+    User.invite!({email: invite.email}, executor)
   end
 end
