1659531f015c369741c7ef3b9c19cf6338ecc847
markevich <slava.markevich@gmail.com>
2013-05-30 12:35:54 +0300

    refactor factory spec

--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe 'validate FactoryGirl factories' do
   FactoryGirl.factories.each do |factory|
     context "with factory for :#{factory.name}" do
-      subject { build(factory.name) }
+      let(:instance) { build(factory.name) }
 
       it "is valid" do
-        subject.valid?.should be, subject.errors.full_messages.join(', ')
+        expect(instance).to be_valid, instance.errors.full_messages.join(', ')
       end
     end
   end
