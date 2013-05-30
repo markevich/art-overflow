eaaeac3a68566cd79fd86fdc7f87757367ed9184
markevich <slava.markevich@gmail.com>
2013-05-30 13:21:29 +0300

    fix n+1 for activity trackable

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
   def index
     @activities = PublicActivity::Activity
       .includes(:owner)
+      .includes(:trackable)
       .where(owner_id: current_user.id, owner_type: "User")
       .order("created_at desc")
   end
