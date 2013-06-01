839faf6511d1f9b9c5c8dcf53999792ad64cae89
Sergei Baranov <onemur@gmail.com>
2013-06-01 21:57:11 +0300

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,28 @@
 class UsersController < ApplicationController
-  before_filter :authorize, :except => [:show]
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def authorize
-  	redirect_to(action: :show) unless current_user  		
-  end
+  before_filter :set_model
+  before_filter :check_if_signed_in, :check_self_request, only: [:follow, :stop_following]
 
   def follow
-    user = User.find(params[:id])
-    if current_user == user
-      redirect_to action: :show
-      return
-    end
-    current_user.follow(user)
-
+    current_user.follow(@user)
     redirect_to action: :show
   end
 
   def stop_following
-    user = User.find(params[:id])
-    if current_user == user
-      redirect_to action: :show
-      return
-    end
-    current_user.stop_following(user)
-
+    current_user.stop_following(@user)
     redirect_to action: :show
+  end
+
+  private
+
+  def check_if_signed_in
+  	render(status: 401, text: 'not authorized') unless user_signed_in?  		
+  end
+
+  def set_model
+    @user = User.find(params[:id])
+  end
+
+  def check_self_request
+    redirect_to action: :show if current_user == @user
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,24 +10,36 @@ describe UsersController do
   
   def follow_user
     post :follow, id: another_user.id
+  end    
+
+  def follow_self
+    post :follow, id: user.id
   end  
 
   describe "#follow" do
     it { expect{ follow_user }.to change(user, :follow_count).by(1) }
     it { expect{ follow_user }.to change(another_user, :followers_count).by(1) }
     it { expect(follow_user).to redirect_to(action: :show) }
-    it { expect(post :follow, id: user.id).to redirect_to(action: :show) }
+
+    it { expect { follow_self }.to_not change(user, :follow_count).by(1) }
+    it { expect(follow_self).to redirect_to(action: :show) }
   end
 
   describe "#stop_following" do
     before { follow_user }
     def stop_following_user
       post :stop_following, id: another_user.id
+    end    
+
+    def stop_following_self
+      post :stop_following, id: user.id
     end
 
     it { expect{ stop_following_user }.to change(user, :follow_count).by(-1) }
     it { expect{ stop_following_user }.to change(another_user, :followers_count).by(-1) }
     it { expect(stop_following_user).to redirect_to(action: :show) }
-    it { expect(post :stop_following, id: user.id).to redirect_to(action: :show) }
+
+    it { expect { stop_following_self }.to_not change(user, :follow_count).by(1) }
+    it { expect(stop_following_self).to redirect_to(action: :show) }
   end
 end
