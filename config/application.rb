40963cca1e5699a90f860de2ec90bdf245975df7
markevich <slava.markevich@gmail.com>
2013-06-10 15:45:20 +0300

    finaly compile jbar

--- config/application.rb
+++ config/application.rb
@@ -10,21 +10,10 @@ require "sprockets/railtie"
 # Assets should be precompiled for production (so we don't need the gems loaded then)
 Bundler.require(*Rails.groups(assets: %w(development test)))
 
-def compile_asset?(path)
-  # ignores any filename that begins with '_' (e.g. sass partials)
-  # all other css/js/sass/image files are processed
-  if File.basename(path) =~ /^[^_].*\.\w+$/
-    puts "Compiling: #{path}"
-    true
-  else
-    puts "Ignoring: #{path}"
-    false
-  end
-end
-
 module ArtOverflow
   class Application < Rails::Application
     config.i18n.default_locale = :ru
-    config.assets.precompile = [ method(:compile_asset?).to_proc ]
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*jbar.js *jbar.css)
   end
 end
