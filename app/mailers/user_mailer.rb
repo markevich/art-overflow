85b560eddc47a757375a1a2eb603f3c2888bb3d6
markevich <slava.markevich@gmail.com>
2013-06-24 11:51:42 +0300

    fix "from" field in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@artoverflow.com"
+  default from: "Art Overflow <info@artoverflow.com>"
   def welcome_email
-    mail(to: 'onemur@gmail.com', subject: 'Hi!')
+    mail(to: ['slava.markevich@gmail.com', 'onemur@gmail.com'], subject: 'Hi!')
   end
 end
