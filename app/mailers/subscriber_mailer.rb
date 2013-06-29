ea5f63f5b2bd26fa68924c63b236b84288479332
markevich <slava.markevich@gmail.com>
2013-06-29 11:16:47 +0300

    update mail message

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,4 +1,4 @@
-class UserMailer < ActionMailer::Base
+class SubscriberMailer < ActionMailer::Base
   default from: "Art Overflow <info@artoverflow.com>"
   def welcome_email
     mail(to: ['slava.markevich@gmail.com', 'onemur@gmail.com'], subject: 'Hi!')
