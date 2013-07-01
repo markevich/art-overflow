80a78f0f9311b5bc8601be97d6b0b3aae0ae2deb
markevich <slava.markevich@gmail.com>
2013-07-01 18:15:22 +0300

    fix seeds

--- db/schema.rb
+++ db/schema.rb
@@ -133,12 +133,12 @@ ActiveRecord::Schema.define(version: 20130629192113) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",                 default: ""
+    t.string   "email",                   default: "", null: false
+    t.string   "encrypted_password",      default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",           default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -149,25 +149,19 @@ ActiveRecord::Schema.define(version: 20130629192113) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "invitation_token",        limit: 60
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
     t.string   "role"
-    t.string   "name"
-    t.integer  "my_comments_count",                  default: 0
-    t.integer  "draft_comcoms_count",                default: 0
-    t.integer  "published_comcoms_count",            default: 0
-    t.integer  "deleted_comcoms_count",              default: 0
-    t.integer  "spam_comcoms_count",                 default: 0
+    t.string   "first_name"
+    t.integer  "my_comments_count",       default: 0
+    t.integer  "draft_comcoms_count",     default: 0
+    t.integer  "published_comcoms_count", default: 0
+    t.integer  "deleted_comcoms_count",   default: 0
+    t.integer  "spam_comcoms_count",      default: 0
+    t.string   "last_name"
+    t.string   "nickname"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "votes", force: true do |t|
