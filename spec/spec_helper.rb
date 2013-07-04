e247de2e473547098745e70195707560f0aa7f1b
markevich <slava.markevich@gmail.com>
2013-07-04 12:15:13 +0300

    add default host for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,14 +22,7 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-
+    Rails.application.routes.default_url_options[:host] = 'test.host'
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
