cf486abdef7418bed6fdc665eee3490b5121c76e
markevich <slava.markevich@gmail.com>
2013-07-04 17:49:10 +0300

    refactor subscriber_controller

--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -7,11 +7,21 @@ describe SubscribersController do
       SecureRandom.stub(:urlsafe_base64).and_return 'secure_token'
       create(:subscriber, id: id)
     end
-    it { expect{ get :unsubscribe, id: 666 }.to raise_error ActiveRecord::RecordNotFound }
+    it 'show 404 unless user exists' do
+      expect{ get :unsubscribe, id: 666 }.to raise_error ActiveRecord::RecordNotFound
+    end
 
-    it { expect{ get :unsubscribe, id: id}.to raise_error ActiveRecord::RecordNotFound }
-    it { expect{ get :unsubscribe, id: id, token: 'incorrect_token' }.to raise_error ActiveRecord::RecordNotFound }
-    it { expect{ get :unsubscribe, id: id, token: token }.to_not raise_error ActiveRecord::RecordNotFound }
+    it 'show 404 if token is nil' do
+      expect{ get :unsubscribe, id: id}.to raise_error ActiveRecord::RecordNotFound
+    end
+
+    it 'show 404 if token is incorrect' do
+      expect{ get :unsubscribe, id: id, token: 'incorrect_token' }.to raise_error ActiveRecord::RecordNotFound
+    end
+
+    it 'doesn\'t fail if params are correct' do
+      expect{ get :unsubscribe, id: id, token: token }.to_not raise_error ActiveRecord::RecordNotFound
+    end
 
     it 'destroy subscriber' do
       get :unsubscribe, id: id, token: token
