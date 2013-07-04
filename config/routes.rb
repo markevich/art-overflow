beed0981b3b512d87d6bf3c80761421380c466ba
markevich <slava.markevich@gmail.com>
2013-07-04 15:26:46 +0300

    create custom devise controllers for after_sign** filters

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ArtOverflow::Application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   unless Rails.env.production?
-    devise_for :users
+    devise_for :users, controllers: { registrations: 'registrations', confirmations: 'confirmations' }
     resources :users do
      member do
         post :follow
