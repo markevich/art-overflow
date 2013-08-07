67e51f5c9f0c3608dc942d45338c66d52c53b716
markevich <slava.markevich@gmail.com>
2013-08-08 00:24:26 +0300

    update commetable

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -2,7 +2,9 @@ module Commentable
   extend ActiveSupport::Concern
 
   included do
-    has_many :comments, as: :commentable
+    has_many :comments, as: :commentable, dependent: :destroy
+    accepts_nested_attributes_for :comments
+    validates_associated :comments
   end
 
 end
--- spec/support/shared_examples/commentable.rb
+++ spec/support/shared_examples/commentable.rb
@@ -1,4 +1,4 @@
 shared_examples_for 'commentable' do
-  it { should have_many(:comments) }
-
+  it { should have_many(:comments).dependent(:destroy) }
+  it { should accept_nested_attributes_for(:comments) }
 end
