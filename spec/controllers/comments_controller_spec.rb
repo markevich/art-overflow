6f6421257259a4da85335dcb259d12aff7e225cf
markevich <slava.markevich@gmail.com>
2013-08-13 21:55:47 +0300

    add test for like/unlike

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -16,4 +16,17 @@ describe CommentsController do
 
     it { expect { post :create, params }.to change(picture.comments, :count).by(1) }
   end
+
+  context '.like' do
+    let!(:comment) { create(:comment, commentable_type: picture.class.to_s, commentable_id: picture.id) }
+
+    it { expect { post :like, id: comment.id }.to change(comment, :votes_for).by(1) }
+  end
+
+  context '.like' do
+    let!(:comment) { create(:comment, commentable_type: picture.class.to_s, commentable_id: picture.id) }
+    before { user.vote_for(comment) }
+
+    it { expect { post :unlike, id: comment.id }.to change(comment, :votes_for).by(-1) }
+  end
 end
