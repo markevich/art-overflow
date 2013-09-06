8a5e376fb5eae115b33ccb63a78b6f8b3a6cf1b0
markevich <slava.markevich@gmail.com>
2013-09-06 21:04:39 +0300

    merge fixes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,7 +18,7 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
     @comments = @picture.comments.includes(:user)
     if user_signed_in?
-      @voted = current_user.voted_on?(@picture)
+      @voted = current_user.liked?(@picture)
       @following = current_user.following?(@picture.user)
       @current_user_id = current_user.id
     end
