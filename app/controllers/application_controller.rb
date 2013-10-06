4ae80e3e2f64228024464aba0d3ccf804848fe9d
markevich <slava.markevich@gmail.com>
2013-10-07 00:14:19 +0300

    indentation fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,11 @@ class ApplicationController < ActionController::Base
   end
 
   def store_location
-   # store last url - this is needed for post-login redirect to whatever the user last visited.
-      if (request.fullpath != "/users/sign_in" && \
-          request.fullpath != "/users/sign_up" && \
-          !request.xhr?) # don't store ajax calls
-        session[:previous_url] = request.fullpath
-      end
+    if request.fullpath != "/users/sign_in" && \
+        request.fullpath != "/users/sign_up" && \
+        !request.xhr?
+      session[:previous_url] = request.fullpath
+    end
   end
 
   def after_sign_in_path_for(resource)
