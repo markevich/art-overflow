4699d76a671e49be28c041ead8413d749ba5d8a2
markevich <slava.markevich@gmail.com>
2014-04-18 13:23:24 +0300

    interlace uploaded pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def active_link_to(name, path, options = {})
-    options[:class] = 'active' if current_page?(path)
+    options[:class] = 'active' if request.fullpath == path
 
     content_tag(:li, class: options[:class]) do
       link_to(name, path, options)
