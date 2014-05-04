b614fb5796cda8aca4c5e6975e6b78454d7deb09
markevich <slava.markevich@gmail.com>
2014-05-04 15:46:52 +0300

    bugfixes

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,11 +2,6 @@ class Follow < ActiveRecord::Base
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
 
-  belongs_to :followable, :polymorphic => true
-  belongs_to :follower,   :polymorphic => true
-
-  def block!
-    self.update_attribute(:blocked, true)
-  end
-
+  belongs_to :followable, polymorphic: true, touch: true
+  belongs_to :follower, polymorphic: true, touch: true
 end
