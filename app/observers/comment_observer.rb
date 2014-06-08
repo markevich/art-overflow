e62c1b3794816aad91e19425dffb1ae5e7964c6c
Sergei Baranov <onemur@gmail.com>
2014-06-08 23:23:50 +0300

    Upgrade observers. Add specs to mailers.

--- app/observers/comment_observer.rb
+++ app/observers/comment_observer.rb
@@ -1,9 +1,10 @@
 class CommentObserver < ActiveRecord::Observer
   def after_commit(comment)
-    if comment.created_at == comment.updated_at
-      if comment.recipient.notification_settings.comments
-        NotificationWorker.perform_async(:new_comment, comment.id)
-      end
+    return unless comment.persisted?
+    return unless comment.created_at == comment.updated_at
+
+    if comment.recipient.notification_settings.comments
+      NotificationWorker.perform_async(:new_comment, comment.id)
     end
   end
 end
--- app/observers/follow_observer.rb
+++ app/observers/follow_observer.rb
@@ -1,9 +1,10 @@
 class FollowObserver < ActiveRecord::Observer
   def after_commit(follow)
-    if follow.created_at == follow.updated_at
-      if follow.recipient.notification_settings.subscribers
-        NotificationWorker.perform_async(:new_follower, follow.id)
-      end
+    return unless follow.persisted?
+    return unless follow.created_at == follow.updated_at
+
+    if follow.recipient.notification_settings.subscribers
+      NotificationWorker.perform_async(:new_follower, follow.id)
     end
   end
 end
