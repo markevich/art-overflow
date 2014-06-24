aab8855a27948eb58257df9def59a360c5b9f6a9
Sergei Baranov <onemur@gmail.com>
2014-06-24 11:04:56 +0300

    Bugfix for tags with html format and notification settings.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,7 @@ class TagsController < ApplicationController
     @tags = ActsAsTaggableOn::Tag.where("tags.name LIKE ?", "%#{params[:term]}%").pluck(:name)
     respond_to do |format|
       format.json { render json: @tags }
+      format.html { redirect_to root_path }
     end
   end
 end
