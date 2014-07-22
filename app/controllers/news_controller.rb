9a4ce2fac4edfaa0f4534ca11ed505a4eedf49f4
markevich <slava.markevich@gmail.com>
2014-07-22 15:38:23 +0300

    display only published news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,11 @@
 class NewsController < InheritedResources::Base
+
+  protected
+
+  def collection
+    @collection ||= end_of_association_chain.published
+  end
+
   def permitted_params
     params.permit(news: [:title, :body, :published, :picture, :short_body])
   end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,6 @@
 class News < ActiveRecord::Base
+  scope :published, -> { where(published: true) }
+
   validates :title, :body, :picture, :short_body, presence: true
 
   mount_uploader :picture, NewsUploader
